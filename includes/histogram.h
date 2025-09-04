17f02d0298049b9e4c9cade81dfc4da4ea3528bc
Gustavo Vilela Mitraud <114029408+Gustavo-V-M@users.noreply.github.com>
2025-09-03 22:47:07 -0300

    feat: started histogram implementation

--- includes/histogram.h
+++ includes/histogram.h
@@ -2,3 +2,15 @@
 #define HISTOGRAM_H
 
 
+class Histogram {
+    public:
+        Histogram(SDL_Surface* surface);
+        ~Histogram();
+
+        bool is_high_contrast();
+        bool is_dark();
+    private:
+        int histogram[256];
+};
+
+#endif
--- src/histogram.cpp
+++ src/histogram.cpp
@@ -2,3 +2,14 @@
 #include <SDL3/SDL.h>
 
 
+Histogram::Histogram(SDL_Surface* surface) {
+    for (int i = 0; i < 256; i++) {
+        histogram[i] = 0;
+    }
+    // TODO: finalizar implementação do construtor
+}
+
+Histogram::~Histogram() {};
+
+bool Histogram::is_high_contrast() { return false; } // TODO: implementar
+bool Histogram::is_dark() { return false; } // TODO: implementar
