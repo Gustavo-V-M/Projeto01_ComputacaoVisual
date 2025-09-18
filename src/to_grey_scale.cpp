cdf6dca3151e813f199a93212f88d4978247608f
macaaalm <h3019123456789@gmail.com>
2025-09-17 22:54:44 -0300

    Criacao da branch para conversao de imagem para escala cinza

--- src/to_grey_scale.cpp
+++ src/to_grey_scale.cpp
@@ -18,17 +18,16 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_surface.h>
 
-// Converts an SDL_Surface to greyscale in-place
-void to_grey_scale(SDL_Surface* surface) {
+static void to_grey_scale(SDL_Surface* surface) {
     if (!surface) return;
 
     SDL_LockSurface(surface);
 
     const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(surface->format);
-    
+
     Uint8 r, g, b, a;
     Uint32* pixels = (Uint32*)surface->pixels;
-    int pixel_count = (surface->w) * (surface->h);
+    const int pixel_count = surface->w * surface->h;
 
     for (int i = 0; i < pixel_count; ++i) {
         SDL_GetRGBA(pixels[i], format, NULL, &r, &g, &b, &a);
@@ -37,4 +36,27 @@ void to_grey_scale(SDL_Surface* surface) {
     }
 
     SDL_UnlockSurface(surface);
+}
+
+static bool is_grey_scale(SDL_Surface* surface) {
+    if (!surface) return false;
+
+    SDL_LockSurface(surface);
+
+    const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(surface->format);
+
+    Uint8 r, g, b, a;
+    Uint32* pixels = (Uint32*)surface->pixels;
+    const int pixel_count = surface->w * surface->h;
+
+    for (int i = 0; i < pixel_count; ++i) {
+        SDL_GetRGBA(pixels[i], format, NULL, &r, &g, &b, &a);
+        if (!(r == g && g == b)) {
+            SDL_UnlockSurface(surface);
+            return false;
+        }
+    }
+
+    SDL_UnlockSurface(surface);
+    return true;
 }
