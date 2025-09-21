ae34cc0171df260bd3f08b52585c47287defd379
Sabrina Midori <143453153+SabrinaMFTC@users.noreply.github.com>
2025-09-21 17:04:24 -0300

    update histogram to include group members RA

--- src/histogram.cpp
+++ src/histogram.cpp
@@ -1,25 +1,10 @@
 /* Group Members:
- * Enzo Guarnieri                               RA
- * Júlia Campolim Oste                          RA
- * Gustavo Villela Mitraud                      RA
- * Matheus Marçal Ramos de Oliveira             RA
+ * Enzo Guarnieri                               RA 10410074
+ * Júlia Campolim Oste                          RA 10408802
+ * Gustavo Villela Mitraud                      RA 10400866
+ * Matheus Marçal Ramos de Oliveira             RA 10409001
  * Sabrina Midori Futami Teixeira de Carvalho   RA 10410220
- *
- * Class 07N - Subject: Visual Computation - Project n. 01
- * Teacher: Andre Kishimoto
- *
- * Refferences:
- * Official SDL Wiki: https://wiki.libsdl.org/SDL3/
- * Official SDL_ttf Documentation: https://www.libsdl.org/projects/old/SDL_ttf/docs/SDL_ttf.html
- * Book:
- * GONZALEZ, R. C.; WOODS, R. E. Processamento digital de imagens, 3ª edição.
- * Youtube Channels:
- * https://youtube.com/playlist?list=PLvv0ScY6vfd-RZSmGbLkZvkgec6lJ0BfX&si=yqz1Lqy13JvwXYoc
- * https://youtube.com/playlist?list=PLvv0ScY6vfd-RZSmGbLkZvkgec6lJ0BfX&si=iSFdVsWFjUHksRBu
- * Font:
- * https://fonts.google.com/specimen/Roboto?preview.text=Whereas%20recognition%20of%20the%20inherent%20dignity
  */
-
 #include <histogram.h>
 #include <SDL3/SDL.h>
 #include <cmath>
