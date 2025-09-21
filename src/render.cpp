c574e030b7b3e8f314ac4363345c856817e7374f
Sabrina Midori <143453153+SabrinaMFTC@users.noreply.github.com>
2025-09-21 17:03:47 -0300

    update render to include group members RA

--- src/render.cpp
+++ src/render.cpp
@@ -1,3 +1,10 @@
+/* Group Members:
+ * Enzo Guarnieri                               RA 10410074
+ * Júlia Campolim Oste                          RA 10408802
+ * Gustavo Villela Mitraud                      RA 10400866
+ * Matheus Marçal Ramos de Oliveira             RA 10409001
+ * Sabrina Midori Futami Teixeira de Carvalho   RA 10410220
+ */
 #include <iostream>
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
